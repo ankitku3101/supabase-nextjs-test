58df299177b59195daaaa47733d2182b7e7b4498
ankitku3101 <ankit.ku3101@gmail.com>
2025-07-26 14:28:37 +0530

    feat: add shadcn and ConfessionForm

---
+++ components/ConfessionForm.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useState } from "react"
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+
+export default function ConfessionForm () {
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        await fetch('/api/confess', {
+            method: 'POST',
+            body: JSON.stringify({message}),
+        })
+        setMessage('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="w-1/2 h-1/2 space-y-4 flex flex-col items-center">
+            <Textarea
+                value={message}
+                onChange={(e) => {
+                    setMessage(e.target.value)
+                }}
+                placeholder="Tell me a secret"
+                className="w-full h-40 p-2 border rounded"
+                required
+            />
+            <Button
+                type="submit"
+                className="px-4 py-2 rounded"
+            >
+                Confess It !
+            </Button>
+        </form>
+    )
+}
